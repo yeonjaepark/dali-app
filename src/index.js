739b1da413e70dbcd8d63c3ad91528592a228100
Yeonjae Park <yeonjaepark@yeonjaes-air.wireless.dartmouth.edu>
2017-04-12 17:31:43 -0400

    sa3

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// change require to es6 import style
+import $ from 'jquery';
+import './style.scss';
+
+let num = 0;
+
+setInterval(() => {
+  $('#main').html(`You've been on this page for ${num} seconds`);
+  num += 1;
+}, 1000);
