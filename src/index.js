4c43d4c71e9247bbc81881319247737746057666
Yeonjae Park <yeonjae.park.19@dartmouth.edu>
2017-04-20 18:14:28 -0400

    new

--- src/index.js
+++ src/index.js
@@ -1,45 +1,8 @@
-import React, { Component } from 'react';
-import debounce from 'lodash.debounce';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/search_bar';
-import youtubeSearch from './youtube-api';
-import VideoList from './components/video_list';
-import VideoDetail from './components/video_detail';
-import './style.scss';
 
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      videos: [],
-      selectedVideo: null,
-    };
-
-    this.search = debounce(this.search, 300);
-  }
-
-  search(text) {
-    youtubeSearch(text).then((videos) => {
-      this.setState({
-        videos,
-        selectedVideo: videos[0],
-      });
-    });
-  }
-
-  render() {
-    return (
-      <div className="main">
-        <SearchBar onSearchChange={text => this.search(text)} />
-        <div id="video-section">
-          <VideoDetail video={this.state.selectedVideo} />
-          <VideoList className="list" onVideoSelect={selectedVideo => this.setState({ selectedVideo })} videos={this.state.videos} />
-        </div>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return <div className="test">All the React are belong to us!</div>;
+};
 
 ReactDOM.render(<App />, document.getElementById('main'));
