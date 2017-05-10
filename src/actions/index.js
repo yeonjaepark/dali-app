475a96b663cb472efa02239e8d1bc1c2badad82a
Yeonjae Park <yeonjae.park.19@dartmouth.edu>
2017-05-10 02:07:02 -0400

    changed root to have /api

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,7 +9,7 @@ export const ActionTypes = {
   DELETE_POST: 'DELETE_POST',
 };
 
-const ROOT_URL = 'https://lab5-yeonjaepark.herokuapp.com/';
+const ROOT_URL = 'https://lab5-yeonjaepark.herokuapp.com/api';
 const API_KEY = '?key=y_park';
 
 export function fetchPosts() {
